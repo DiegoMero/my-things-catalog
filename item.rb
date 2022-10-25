1b505a13e0aee9936aa46b95aff84843efabcfc4
DiegoMero <diegomero_1999@hotmail.com>
2022-10-24 22:49:36 -0500

    Add item class methods

--- item.rb
+++ item.rb
@@ -1,11 +1,12 @@
 require 'securerandom'
+require 'date'
 
 class Item
-  attr_accessor :publish_date
+  attr_accessor :publish_date, :archived
 
   def initialize(publish_date, archived: false)
     @id = SecureRandom.random_number(1..100)
-    @publish_date = publish_date
+    @publish_date = Date.parse(publish_date)
     @archived = archived
   end
 
@@ -25,7 +26,12 @@ class Item
     @label = label
   end
 
-  def can_be_archived?; end
+  def can_be_archived?
+    date_to_check = Date.parse("#{Date.today.year - 10}-#{Date.today.mon}-#{Date.today.mday}")
+    @publish_date < date_to_check
+  end
 
-  def move_to_archive; end
+  def move_to_archive
+    @archived = true if can_be_archived? == true
+  end
 end
