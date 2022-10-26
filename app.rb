a6cd9452448c0fc511dbad1470cf9080edf3508d
DiegoMero <diegomero_1999@hotmail.com>
2022-10-26 00:31:46 -0500

    Add json files to preserve data

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require './book'
 require './label'
+require 'json'
 
 class App
   def initialize
@@ -32,6 +33,30 @@ class App
   end
 
   def labels_list
-    @labels.each_with_index { |label, index| puts "Label #{index + 1}) Title: #{label.title}" }
+    @labels.each_with_index { |label, index| puts "Label #{index + 1}) Title: #{label.title}, Color: #{label.color}" }
+  end
+
+  def save_data
+    @books = @books.map do |book|
+      { publisher: book.publisher, cover_state: book.cover_state, publish_date: book.publish_date }
+    end
+    @books = JSON.generate(@books)
+    File.write('data/books.json', @books)
+
+    @labels = @labels.map { |label| { title: label.title, color: label.color } }
+    @labels = JSON.generate(@labels)
+    File.write('data/labels.json', @labels)
+  end
+
+  def load_data
+    book_data = File.open('data/books.json')
+    book_data = book_data.read
+    book_data = JSON.parse(book_data)
+    book_data.map { |book| @books.push(Book.new(book['publisher'], book['cover_state'], book['publish_date'])) }
+
+    label_data = File.open('data/labels.json')
+    label_data = label_data.read
+    label_data = JSON.parse(label_data)
+    label_data.map { |label| @labels.push(Label.new(label['title'], label['color'])) }
   end
 end
