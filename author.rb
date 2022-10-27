7da1b9614a29985ba3092233d02e7fa10b203342
DiegoMero <diegomero_1999@hotmail.com>
2022-10-27 17:33:24 -0500

    Fix linter errors

--- author.rb
+++ author.rb
@@ -4,7 +4,7 @@ require './item'
 class Author
   def initialize(first_name, last_name)
     @id = SecureRandom.random_number(1..100)
-    @first_name= first_name
+    @first_name = first_name
     @last_name = last_name
     @items = []
   end
