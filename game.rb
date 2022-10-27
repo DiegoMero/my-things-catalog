6a83cda83345745c0b463f4a95221871f2976c4e
DiegoMero <diegomero_1999@hotmail.com>
2022-10-27 17:53:16 -0500

    Add game and author methods

--- game.rb
+++ game.rb
@@ -1,6 +1,15 @@
-class Game
-  def initialize(multiplayer, last_played_at)
+require_relative './item'
+require 'date'
+
+class Game < Item
+  def initialize(multiplayer, last_played_at, publish_date, archived: false)
+    super(publish_date, archived: archived)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
+  end
+
+  def can_be_archived?
+    two_years_old_date = Date.parse("#{Date.today.year - 2}-#{Date.today.mon}-#{Date.today.mday}")
+    super && last_played_at < two_years_old_date
   end
 end
