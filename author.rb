4f2569beb42ec08348455a2612f224aee4e3aa1f
DiegoMero <diegomero_1999@hotmail.com>
2022-10-27 17:32:20 -0500

    Add game and author class

---
+++ author.rb
@@ -0,0 +1,11 @@
+require 'securerandom'
+require './item'
+
+class Author
+  def initialize(first_name, last_name)
+    @id = SecureRandom.random_number(1..100)
+    @first_name= first_name
+    @last_name = last_name
+    @items = []
+  end
+end
